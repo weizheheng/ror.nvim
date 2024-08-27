402f2585dae70495b5f33b930f8a32c61152b244
itisbryan <nkhoinguyen26@gmail.com>
2024-08-27 13:11:30 +0700

    FIX | Update list

--- lua/ror/finders.lua
+++ lua/ror/finders.lua
@@ -1,28 +1,43 @@
 local M = {}
 
-
 function M.select_finders()
-  vim.ui.select(
-    { "Models", "Controllers", "Migrations", "Views", "Model tests", "Controller tests", "System tests" },
-    { prompt = "What are you looking for?" },
-    function (finder)
-      if finder == "Models" then
-        require("ror.finders.model").find()
-      elseif finder == "Controllers" then
-        require("ror.finders.controller").find()
-      elseif finder == "Migrations" then
-        require("ror.finders.migration").find()
-      elseif finder == "Views" then
-        require("ror.finders.view").find()
-      elseif finder == "Model tests" then
-        require("ror.finders.model_test").find()
-      elseif finder == "Controller tests" then
-        require("ror.finders.controller_test").find()
-      elseif finder == "System tests" then
-        require("ror.finders.system_test").find()
-      end
-    end
-  )
+	vim.ui.select({
+		"Models",
+		"Controllers",
+		"Migrations",
+		"Views",
+		"Model tests",
+		"Controller tests",
+		"System tests",
+		"Services",
+		"Service tests",
+		"[GRAPHQL] Types",
+		"[GRAPHQL] Mutations",
+	}, { prompt = "What are you looking for?" }, function(finder)
+		if finder == "Models" then
+			require("ror.finders.model").find()
+		elseif finder == "Controllers" then
+			require("ror.finders.controller").find()
+		elseif finder == "Migrations" then
+			require("ror.finders.migration").find()
+		elseif finder == "Views" then
+			require("ror.finders.view").find()
+		elseif finder == "Model tests" then
+			require("ror.finders.model_test").find()
+		elseif finder == "Controller tests" then
+			require("ror.finders.controller_test").find()
+		elseif finder == "System tests" then
+			require("ror.finders.system_test").find()
+		elseif finder == "Services" then
+			require("ror.finders.service").find()
+		elseif finder == "Service Tests" then
+			require("ror.finders.service_test").find()
+		elseif finder == "[GRAPHQL] Types" then
+			require("ror.finders.graphql_type").find()
+		elseif finder == "[GRAPHQL] Mutations" then
+			require("ror.finders.graphql_mutation").find()
+		end
+	end)
 end
 
 return M
